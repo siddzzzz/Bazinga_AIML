5283f29d6ea57d544c2db9042424d009b00947a5
Siddharth Sahay <siddharthsahay2004@gmail.com>
2024-11-17 08:06:48 +0530

    Webpage deployment new3

--- install_chrome.sh
+++ install_chrome.sh
@@ -2,11 +2,12 @@
 # Update package list and install dependencies
 sudo apt update
 sudo apt install -y wget apt-transport-https ca-certificates software-properties-common
-
+echo "trying to install chrome"
 # Add Google Chrome's repository
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 # Install Google Chrome
 sudo apt update
 sudo apt install -y google-chrome-stable
+echo "finished to install chrome"
